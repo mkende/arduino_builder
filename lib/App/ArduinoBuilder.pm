d99102a7a8a1966c4ff138c39d4c8b97f876a3ea
Mathias Kende <mathias@kende.fr>
2023-04-19 13:55:54 +0200

    Minor changes to the script to prepare for future features.

--- lib/App/ArduinoBuilder.pm
+++ lib/App/ArduinoBuilder.pm
@@ -21,19 +21,23 @@ sub Run {
   GetOptions(
       'help|h' => sub { pod2usage(-exitval => 0, -verbose => 2)},
       'project|p=s' => \$project_dir,
-
+      'build|b=s' => \$build_dir,
     ) or pod2usage(-exitval => 2, -verbose =>0);
 
   if ($project_dir && !$build_dir) {
     $build_dir = getcwd();
   } elsif (!$project_dir) {
     $project_dir = getcwd();
-    if (!build_dir) {
+    if (!$build_dir) {
       $build_dir = catdir($project_dir, '_build');
     }
   }
 
-  my $config =
+  my $config = App::ArduinoBuilder::Config->new(
+      files => [catfile($project_dir, 'arduino_builder.local'),
+                catfile($project_dir, 'arduino_builder.config')],
+      resolve => 1);
+  print $config->dump();
 }
 
 1;
